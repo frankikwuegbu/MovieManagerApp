d87360140c51f1a8ff20adbf2e40f16b27ef650c
frankikwuegbu <99530481+frankikwuegbu@users.noreply.github.com>
2026-02-13 08:56:11 +0100

    refactor: add src solution folder

--- src/MovieManager/Controllers/UserController.cs
+++ src/MovieManager/Controllers/UserController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/users")]
     [ApiController]
     public class UserController(ISender sender) : ControllerBase
     {
